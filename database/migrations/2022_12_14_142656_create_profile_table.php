9b028e4b15e504b8356036106588e18ea7404cf5
Rehagana Prananta <rehaganasinulingga@gmail.com>
2023-05-06 11:35:54 +0700

    mengubah controller profile, model roledan user, mengubah migration

---
+++ database/migrations/2022_12_14_142656_create_profile_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']);
+            $table->text('alamat');
+            $table->string('nomor_telepon'); // nomer telepon udah ada
+            $table->foreignId('users_id')->references('id')->on('users'); // foreignId udah termasuk unsigned BigINT
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
