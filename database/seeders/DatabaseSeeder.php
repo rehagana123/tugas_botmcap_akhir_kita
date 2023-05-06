09c276e5e9c047185fb8679dc4c05ba027469598
RahmiHandayani <eruresikzomos@gmail.com>
2023-05-06 11:59:56 +0700

    test seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,43 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Profile;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
         // \App\Models\User::factory(10)->create();
+  Role::create([
+            'nama' => 'admin',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Role::create([
+            'nama' => 'guest',
+        ]);
+
+        $user = User::create([
+            'username' => 'Admin',
+            'email' => 'kelompok@kami.com',
+            'password' => bcrypt('admin1234'),
+            'role_id' => 1,
+        ]);
+
+        Profile::create([
+            'nama' => 'kelompok kami',
+            'jenis_kelamin' => 'laki-laki',
+            'alamat' => 'jalan di penjaitan',
+            'nomor_telepon' => 8427215757,
+            'users_id' => $user->id
+        ]);
     }
 }
